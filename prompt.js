dc858216cd859cbd71bf79d98ae3e3df81d39f92
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-01-11 11:59:50 +0100

    Add files via upload

---
+++ prompt.js
@@ -0,0 +1,21 @@
+var Numbers= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+var ShowPyramid = document.getElementById("ShowPyramid");
+ShowPyramid.onclick = function(){
+  Pyramid()
+};
+
+function Pyramid() {
+  var output = '';
+  var Rows = prompt("Enter a number", "20");
+  for (var i = 1; i <= Rows; i++) {
+      for (var j = 1; j <= i; j++) {
+          output += j + '  ';
+      }
+      document.write(output);
+      document.write("<br>");
+      output = '';
+  }
+}
+
+Pyramid();
